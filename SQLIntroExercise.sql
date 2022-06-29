b95e1e318e9853e7bfe9b03fe8a609a1338c3867
David Taccetta <taccetta14@gmail.com>
2022-06-29 12:27:48 -0400

    Made CHanges During Review

--- SQLIntroExercise.sql
+++ SQLIntroExercise.sql
@@ -1,38 +1,41 @@
+/*Went in and made changes 
+to simplify after review*/
+
 -- find all products
 SELECT * FROM products;
 
 -- find all products that cost $1400
-SELECT * FROM products WHERE products.Price = 1400;
+SELECT * FROM products WHERE Price = 1400;
 
 -- find all products that cost $11.99 or $13.99
-SELECT * FROM products WHERE products.Price = 11.99 OR products.Price = 13.99 ORDER BY products.Price DESC;
+SELECT * FROM products WHERE Price = 11.99 OR Price = 13.99 ORDER BY Price DESC;
 
 -- find all products that do NOT cost 11.99 - using NOT
-SELECT * FROM products WHERE NOT products.Price = 11.99 ORDER BY products.Price;
+SELECT * FROM products WHERE NOT Price = 11.99 ORDER BY Price;
 
 -- find all products and sort them by price from greatest to least
-SELECT * FROM products ORDER BY products.Price DESC;
+SELECT * FROM products ORDER BY Price DESC;
 
 -- find all employees who don't have a middle initial
-SELECT employees.FirstName, employees.MiddleInitial, employees.LastName FROM employees WHERE employees.MiddleInitial IS NULL;
+SELECT FirstName, MiddleInitial, LastName FROM employees WHERE MiddleInitial IS NULL;
 
 -- find distinct product prices
-SELECT DISTINCT products.Price FROM products ORDER BY products.Price;
+SELECT DISTINCT Price FROM products ORDER BY Price;
 
 -- find all employees whose first name starts with the letter ‘j’
-SELECT * FROM employees WHERE employees.FirstName LIKE 'j%';
+SELECT * FROM employees WHERE FirstName LIKE 'j%' ORDER BY FirstName;
 
 -- find all Macbooksproducts
-SELECT * FROM products WHERE products.Name LIKE 'Mac%';
+SELECT * FROM products WHERE Name LIKE 'Mac%';
 
 -- find all products that are on sale
-SELECT * FROM products WHERE products.OnSale = 1 ORDER BY products.Price;
+SELECT * FROM products WHERE OnSale = 1 ORDER BY Price;
 
 -- find the average price of all products
-SELECT AVG (products.Price) FROM products;
+SELECT AVG (Price) FROM products;
 
 -- find all Geek Squad employees who don't have a middle initial
-SELECT employees.FirstName, employees.MiddleInitial, employees.LastName, employees.Title FROM employees WHERE employees.Title LIKE 'GEEK%' AND employees.MiddleInitial IS NULL;
+SELECT FirstName, MiddleInitial, LastName, Title FROM employees WHERE Title LIKE 'GEEK%' AND MiddleInitial IS NULL ORDER BY FirstName;
 
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword
-SELECT * FROM products WHERE products.StockLevel BETWEEN 500 AND 1200 ORDER BY products.Price;
+SELECT * FROM products WHERE StockLevel BETWEEN 500 AND 1200 ORDER BY Price;
