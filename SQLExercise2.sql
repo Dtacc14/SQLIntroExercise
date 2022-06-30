408812a872a76a665ac9c248e92b623075cedd5d
David Taccetta <taccetta14@gmail.com>
2022-06-30 11:25:47 -0400

    Initial SQLExercise2 Commit

---
+++ SQLExercise2.sql
@@ -0,0 +1,45 @@
+/* joins: select all the computers from the products table:
+using the products table and the categories table, return the product name and the category name */
+
+SELECT products.Name, categories.CategoryID 
+FROM products 
+LEFT JOIN categories ON categories.CategoryID = products.CategoryID
+WHERE categories.CategoryID = 1;
+
+/* joins: find all product names, product prices, and products ratings that have a rating of 5 */
+
+SELECT products.Name, products.Price, reviews.Rating 
+FROM products 
+LEFT JOIN reviews on reviews.Rating = 5;
+ 
+/* joins: find the employee with the most total quantity sold.  use the sum() function and group by */
+SELECT employees.FirstName AS 'NAME', MAX(sales.Quantity) AS "QUANTITY" 
+FROM employees 
+LEFT JOIN sales on employees.EmployeeID = sales.EmployeeID;
+
+/* joins: find the name of the department, and the name of the category for Appliances and Games */
+SELECT departments.Name, categories.Name
+FROM departments 
+INNER JOIN categories on categories.DepartmentID = departments.DepartmentID
+WHERE categories.name = 'Appliances' OR categories.name = 'Games';
+
+/* joins: find the product name, total # sold, and total price sold,
+ for Eagles: Hotel California --You may need to use SUM() */
+ SELECT products.Name,
+ SUM(sales.Quantity) AS 'TOTAL SOLD',
+ SUM(sales.PricePerUnit) AS 'TOTAL SOLD'
+ FROM products
+ INNER JOIN sales on products.ProductID = sales.ProductID
+ WHERE products.Name LIKE "%Hotel California%";
+ 
+/* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
+SELECT products.name, reviews.Reviewer, reviews.Rating, reviews.Comment FROM reviews LEFT JOIN products on products.ProductID = 857 WHERE reviews.Rating = 1;
+
+-- ------------------------------------------ Extra - May be difficult
+/* Your goal is to write a query that serves as an employee sales report.
+This query should return the employeeID, the employee's first and last name, the name of each product, how many of that product they sold */
+SELECT employees.FirstName AS 'FIRST', employees.LastName AS 'LAST', employees.EmployeeID AS 'EMP ID', sales.Quantity AS 'SOLD', products.name AS 'PRODUCT'
+from employees
+INNER JOIN sales ON employees.EmployeeID = sales.EmployeeID
+INNER JOIN products ON sales.ProductID = products.ProductID
+ORDER BY employees.LastName;
